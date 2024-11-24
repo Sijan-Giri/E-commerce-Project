c172dbed24c2f1b8c1296d9b4d2fd5bce26ed2e6
Sijan-Giri <girisijan346@gmail.com>
2024-11-24 21:31:58 +0545

    Code refactored

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express,{Application,Request,Response} from "express";
 const app:Application = express();
 
-require("./model/index")
+
+import * as dotenv from "dotenv"
+dotenv.config()
+
+import "./database/connection"
 
 app.get("/",(req:Request,res:Response) => {
     res.send("API is running...")
--- backend/src/model/index.ts
+++ backend/src/model/index.ts
@@ -1,33 +1,33 @@
 import {Sequelize , DataTypes} from "sequelize"
-import dbConfig from "../config/dbConfig"
+// import dbConfig from "../config/dbConfig"
 
-const sequelize = new Sequelize(dbConfig.db,dbConfig.username,dbConfig.password,{
-    host : dbConfig.host,
-    port : 3306,
-    dialect : dbConfig.dialect,
-    pool : {
-        acquire : dbConfig.pool.acquire,
-        min : dbConfig.pool.min,
-        max : dbConfig.pool.max,
-        idle : dbConfig.pool.idle
-    }
-})
+// const sequelize = new Sequelize(dbConfig.db,dbConfig.username,dbConfig.password,{
+//     host : dbConfig.host,
+//     port : 3306,
+//     dialect : dbConfig.dialect,
+//     pool : {
+//         acquire : dbConfig.pool.acquire,
+//         min : dbConfig.pool.min,
+//         max : dbConfig.pool.max,
+//         idle : dbConfig.pool.idle
+//     }
+// })
 
-sequelize
-.authenticate()
-.then(() => {
-    console.log("Database connected successfully")
-})
-.catch((err) => {
-    console.log("Something went wrong!",err)
-})
+// sequelize
+// .authenticate()
+// .then(() => {
+//     console.log("Database connected successfully")
+// })
+// .catch((err) => {
+//     console.log("Something went wrong!",err)
+// })
 
-const db:any = {}
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+// const db:any = {}
+// db.Sequelize = Sequelize
+// db.sequelize = sequelize
 
-db.sequelize.sync({force : false}).then(() => {
-    console.log("Yes migrated")
-})
+// db.sequelize.sync({force : false}).then(() => {
+//     console.log("Yes migrated")
+// })
 
-export default db;
+// export default db;
