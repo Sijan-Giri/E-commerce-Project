dda6cd706fc7edd06075a5d586027367dfc31d47
Sijan-Giri <girisijan346@gmail.com>
2024-12-03 20:31:14 +0545

    addCategory , getSingleCategory , deleteCategory integrated

--- backend/src/controllers/categoryController.ts
+++ backend/src/controllers/categoryController.ts
@@ -1,5 +1,5 @@
 import Category from "../model/category"
-
+import { Request , Response } from "express";
 
 class CategoryController{
     categoryData = [
@@ -21,6 +21,93 @@ class CategoryController{
         }
         else {
             console.log("Category already seeded")
+        }
+    }
+
+    async addCategory(req:Request,res:Response):Promise<void> {
+        const {categoryName} = req.body;
+        if(!categoryName) {
+            res.status(400).json({
+                message : "Please provide category name"
+            })
+            return
+        }
+        const data = await Category.create({
+            categoryName
+        })
+        res.status(200).json({
+            message : "Category created successfully",
+            data : data
+        })
+    }
+
+    async getAllCategory(req:Request,res:Response):Promise<void> {
+        const datas = await Category.findAll();
+        if(datas.length == 0){
+            res.status(404).json({
+                message : "No category found"
+            })
+        }
+        else {
+            res.status(200).json({
+                message : "Category fetched successfully",
+                data : datas
+            })
+        }
+    }
+    async getSingleCategory(req:Request,res:Response):Promise<void> {
+        const {id} = req.params;
+        if(!id) {
+            res.status(400).json({
+                message : "Please provide id"
+            })
+            return
+        }
+        const data = await Category.findAll({
+            where : {
+                id : id
+            }
+        })
+        if(data.length == 0) {
+            res.status(404).json({
+                message : "No category found with this id"
+            })
+        }
+        else {
+            res.status(200).json({
+                message : "Category fetched successfully",
+                data : data
+            })
+        }
+    }
+
+    async deleteCategory(req:Request,res:Response):Promise<void> {
+        const {id} = req.params;
+        if(!id) {
+            res.status(400).json({
+                message : "Please provide id"
+            })
+            return
+        }
+        const data = await Category.findAll({
+            where : {
+                id : id
+            }
+        })
+        if(data.length == 0) {
+            res.status(404).json({
+                message : 'No category found with this id'
+            })
+        }
+        else {
+            await Category.destroy({
+                where : {
+                    id : id
+                }
+            })
+            res.status(200).json({
+                message : "Category deleted successfully"
+            })
         }
     }
 }
