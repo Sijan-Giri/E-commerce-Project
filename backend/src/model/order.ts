bb4b1bc34ba9f5e8a6f944eab8549d377fca3626
Sijan-Giri <girisijan346@gmail.com>
2024-12-16 14:35:11 +0545

    Order table created

---
+++ backend/src/model/order.ts
@@ -0,0 +1,42 @@
+import { Column , Table , DataType , Model } from "sequelize-typescript";
+
+@Table({
+    tableName : "orders",
+    modelName : "Order",
+    timestamps : true
+})
+
+class Order extends Model{
+    @Column({
+        primaryKey : true,
+        type : DataType.UUID,
+        defaultValue : DataType.UUIDV4
+    })
+    declare id : string;
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false
+    })
+    declare phoneNumber : string;
+
+    @Column({
+        type : DataType.STRING,
+        allowNull : false
+    })
+
+    declare shippingAddress : string;
+
+    @Column({
+        type : DataType.FLOAT,
+        allowNull : false
+    })
+    declare totalAmount : number;
+
+    @Column({
+        type : DataType.ENUM('pending','cancelled','delivered','onTheWay','preparation'),
+        defaultValue : "pending",
+        allowNull : false
+    })
+    declare orderStatus : string
+}
